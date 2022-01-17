86315f7a907691efd002cb8ee16516d8f683646d
João Victor Viana Pacheco <diadetedio@outlook.com>
2022-01-17 00:31:50 -0300

    * Implemented receiving mechanism for RetrieveAddresses

--- IzaBlockchain.Net/RequestProcessors/RetrieveAddressesProcessor.cs
+++ IzaBlockchain.Net/RequestProcessors/RetrieveAddressesProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
@@ -15,28 +16,53 @@ namespace IzaBlockchain.Net.RequestProcessors
 
         public override unsafe bool Process(Span<byte> receivedData, PeerConnection fromPeer, TcpClient fromClient)
         {
-            List<Address> addresses = Blockchain.Local.GetData<List<Address>>("LocalAddresses");
-
-            int size = addresses.Count * BlockchainGenerals.AddressSize;
-
-            //Span<byte> pack = stackalloc byte[size];
-            void* packPtr = NativeMemory.Alloc((nuint)size);
-            Span<byte> pack = new Span<byte>(packPtr, size);
-
-            for(int i = 0; i < addresses.Count; i++)
+            // Receive
+            if (receivedData.Length > 0)
             {
-                var address = addresses[i];
+                Span<byte> addressesCountBytes = receivedData.Slice(0, sizeof(int));
+                int addressesCount = BinaryPrimitives.ReadInt32LittleEndian(addressesCountBytes);
 
-                for (int j = 0; j < BlockchainGenerals.AddressSize; j++)
-                    pack[i * BlockchainGenerals.AddressSize] = address.data[j];
+                for(int i = sizeof(int); i < addressesCount; i++)
+                {
+                    Span<byte> addressBytes = receivedData.Slice(i * BlockchainGenerals.AddressSize, BlockchainGenerals.AddressSize);
+                    var address = new Address(addressBytes);
+                    fromPeer.Addresses.Add(address);
+                }
             }
-
-            fromPeer.SendData((addresses, client, stream) =>
+            // Send
+            else
             {
+                List<Address> addresses = Blockchain.Local.GetData<List<Address>>("LocalAddresses");
+
+                int addressesCount = addresses.Count;
+
+                int size = addressesCount * BlockchainGenerals.AddressSize;
+
+                //Span<byte> pack = stackalloc byte[size];
+                void* packPtr = NativeMemory.Alloc((nuint)size);
                 Span<byte> pack = new Span<byte>(packPtr, size);
-                stream.Write(pack);
-                NativeMemory.Free(packPtr);
-            });
+
+                for (int i = 0; i < addressesCount; i++)
+                {
+                    var address = addresses[i];
+
+                    for (int j = 0; j < BlockchainGenerals.AddressSize; j++)
+                        pack[i * BlockchainGenerals.AddressSize] = address.data[j];
+                }
+
+                fromPeer.SendData((addresses, client, stream) =>
+                {
+                    stream.WriteByte((byte)CoreRequestTypes.RetrieveAddresses);
+
+                    Span<byte> addressesCountBytes = stackalloc byte[sizeof(int)];
+                    BinaryPrimitives.WriteInt32LittleEndian(addressesCountBytes, addressesCount);
+                    stream.Write(addressesCountBytes);
+
+                    Span<byte> pack = new Span<byte>(packPtr, size);
+                    stream.Write(pack);
+                    NativeMemory.Free(packPtr);
+                });
+            }
 
             return true;
         }
